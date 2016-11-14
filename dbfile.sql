5254024b36df9b8ea51f0585d5471ce4407eae30
fakedestinyck <fakedestinyck@gmail.com>
2016-11-14 13:33:01 -0500

    Add files via upload

---
+++ dbfile.sql
@@ -0,0 +1,54 @@
+-- MySQL dump 10.13  Distrib 5.7.12, for osx10.9 (x86_64)
+--
+-- Host: localhost    Database: tp
+-- ------------------------------------------------------
+-- Server version	5.7.16
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `comment`
+--
+
+DROP TABLE IF EXISTS `comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `comment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `content` longtext NOT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `tags` varchar(45) DEFAULT NULL,
+  `flag` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comment`
+--
+
+LOCK TABLES `comment` WRITE;
+/*!40000 ALTER TABLE `comment` DISABLE KEYS */;
+INSERT INTO `comment` VALUES (1,'This is the first test message.','2016-11-03 08:29:58','Halloween@%#!|RPI',NULL),(2,'This is the second test message.','2016-11-03 09:23:34','test@%#!|RPI',NULL),(3,'This is a very long test message. This is a very long test message. This is a very long test message.\nThis is a very long test message. This is a very long test message.\nThis is a very long test message.','2016-11-03 09:24:42','long@%#!|RPI',NULL),(4,'A new comment.','2016-11-10 15:29:27',NULL,NULL),(5,'...','2016-11-10 15:42:06',NULL,NULL),(6,'Just a testing message.','2016-11-10 15:42:49',NULL,NULL),(7,'hhhhhhhhhhhhh','2016-11-10 15:46:29',NULL,NULL),(8,'Weird...','2016-11-10 15:48:10',NULL,NULL),(9,'2323','2016-11-10 15:48:25',NULL,NULL),(10,'Test on chrome','2016-11-10 15:48:31',NULL,NULL),(11,'2323','2016-11-10 15:48:59',NULL,NULL),(12,'sdsdsdsdsds','2016-11-10 15:56:23',NULL,NULL),(13,'sdsds11111','2016-11-10 15:59:29',NULL,NULL),(14,'Testing Chrome -S','2016-11-10 16:00:55',NULL,NULL);
+/*!40000 ALTER TABLE `comment` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-11-14 13:32:35
